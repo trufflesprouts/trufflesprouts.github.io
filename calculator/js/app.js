dfc44bf4c451dc3792225a72711f65b044809a43
trufflesprouts <milkyabdullah@gmail.com>
2016-11-24 16:15:14 +0300

    removed soultion to sticky hover

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -87,13 +87,3 @@ for (var i = 0; i < calcBtnELs.length; i++) {
 		});
 	})(i);
 };
-
-// Fix sticky hover style
-function fix()
-{
-    var el = this;
-    var par = el.parentNode;
-    var next = el.nextSibling;
-    par.removeChild(el);
-    setTimeout(function() {par.insertBefore(el, next);}, 0)
-}
