68b031072e1b93c4540b37fde18cbb0889bcc0d3
trufflesprouts <milkyabdullah@gmail.com>
2016-11-24 16:11:48 +0300

    disabled mobile zoom and fixed sticky hover style

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -87,3 +87,13 @@ for (var i = 0; i < calcBtnELs.length; i++) {
 		});
 	})(i);
 };
+
+// Fix sticky hover style
+function fix()
+{
+    var el = this;
+    var par = el.parentNode;
+    var next = el.nextSibling;
+    par.removeChild(el);
+    setTimeout(function() {par.insertBefore(el, next);}, 0)
+}
