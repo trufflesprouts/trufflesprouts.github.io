d2ceacd4ad36de1fc5fde8cfdb30dde131ea263d
trufflesprouts <milkyabdullah@gmail.com>
2017-03-25 18:52:25 +0300

    finished v1

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,9 @@
+
+// document.getElementsByTagName('video')[0].onended = function () {
+//   this.load();
+//   this.play();
+// };
+
 // Hamburger Icon
 
 var hamburgerIcon = document.getElementById('hamburger-icon');
