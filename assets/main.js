d918b4de8df9aea9d04d353970ba53e034e37a6f
trufflesprouts <milkyabdullah@gmail.com>
2017-05-27 17:49:05 +0300

    hamburger update

--- assets/main.js
+++ assets/main.js
@@ -1,24 +1,12 @@
 // Hamburger Icon
+var nav = document.getElementById("headerNav");
 var hamburgerIcon = document.getElementById('hamburger-icon');
 
-hamburgerIcon.addEventListener('click', function(ev) {
-  if (hamburgerIcon.className === 'active') {
-    hamburgerIcon.className = '';
-  } else {
-    hamburgerIcon.className = 'active';
-  }
-});
-
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-var nav = document.getElementById("headerNav");
-
 function hamburgerFunction() {
-  if (nav.className === "nav") {
-    nav.className += " active";
-  } else {
-    nav.className = "nav";
-  }
+  window.scrollTo(0, 0);
+  document.getElementsByTagName('body')[0].classList.toggle('stop-scrolling');
+  nav.classList.toggle("active");
+  hamburgerIcon.classList.toggle("active");
 }
 
 
@@ -28,11 +16,7 @@ for (var i = 0; i < pElms.length; i++) {
     if (pElms[i].firstChild.tagName == "IMG") {
       (function(index) {
         pElms[index].firstChild.addEventListener('click', function() {
-          if (pElms[index].firstChild.className == "clicked") {
-            pElms[index].firstChild.className = "";
-          } else {
-            pElms[index].firstChild.className = "clicked";
-          }
+          pElms[index].firstChild.classList.toggle('clicked');
         });
       })(i);
     }
